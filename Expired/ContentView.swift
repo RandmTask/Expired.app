50b88288b11aaa5d89295ce3377d8b09a77a4584
RandmTask <memnoch@yahoo.com.au>
2026-03-25 12:02:46 +0800

    Initial Commit

---
+++ Expired/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expired
+//
+//  Created by Deon O'Brien on 25/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expired/ExpiredApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpiredApp.swift
+//  Expired
+//
+//  Created by Deon O'Brien on 25/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct ExpiredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
